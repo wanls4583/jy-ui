1979b17335c1417f3459235138e482d74fcfb448
DESKTOP-SI1F6AQ\40905 <409051987@qq.com>
2021-05-17 17:46:33 +0800

    [master]

--- modules/pager.js
+++ modules/pager.js
@@ -5,6 +5,20 @@
  */
 !(function () {
     function factory($, Common) {
+        var pagerClass = 'song-pager';
+        var pagerPrev = 'song-pager-prev';
+        var pagerPrevDisabled = 'song-pager-prev-disabled';
+        var pagerNext = 'song-pager-next';
+        var pagerNextDisabled = 'song-pager-next-disabled';
+        var pagerLimit = 'song-pager-limits';
+        var pagerNums = 'song-pager-nums';
+        var pagerNum = 'song-pager-num';
+        var pagerDot = 'song-pager-dot';
+        var pagerNow = 'song-pager-now';
+        var pagerJump = 'song-pager-jump';
+        var pagerInput = 'song-pager-input';
+        var pagerConfirm = 'song-pager-confirm';
+        var pagerCount = 'song-pager-count';
         var event = Common.getEvent();
         var Pager = {
             render: render,
@@ -36,7 +50,7 @@
             option.next = option.next || '下一页';
             option.size = option.size || 'normal';
             option.filter = $elem.attr('song-filter') || '';
-            $pager = option.$pager || $('<div class="song-pager song-row"></div>');
+            $pager = option.$pager || $('<div class="' + [pagerClass, 'song-row'].join(' ') + '"></div>');
             $pager[0].option = option;
             $pager.html('');
             switch (option.size) {
@@ -49,38 +63,81 @@
                 $pager.insertAfter($elem);
                 $elem.hide();
             }
-            for (var layout_i = 0; layout_i < option.layout.length; layout_i++) {
-                if (option.layout[layout_i] == 'count') {
-                    $pager.append('<span class="song-pager-count">共' + option.count + '条</span>');
+            // 是否显示总数
+            if (option.layout.indexOf('count') > -1) {
+                if (!option.$count) {
+                    option.$count = $('<span class="' + pagerCount + '"></span>');
+                    $pager.append(option.$count);
                 }
-                if (option.layout[layout_i] == 'prev') {
-                    $pager.append('<a href="javascript:;" class="song-pager-prev ' + (option.nowPage == 1 ? 'song-pager-prev-disabled' : '') + '">' + (option.prev) + '</a>');
-                }
-                if (option.layout[layout_i] == 'page') {
-                    $pager.append('<div class="song-pager-nums"></div>')
-                    renderNowPage(option);
-                }
-                if (option.layout[layout_i] == 'next') {
-                    $pager.append('<a href="javascript:;" class="song-pager-next ' + (option.nowPage == option.pages ? 'song-pager-next-disabled' : '') + '">' + (option.next) + '</a>');
-                }
-                if (option.layout[layout_i] == 'limit') {
-                    var $select = $('<select class="song-pager-limits" song-ignore></select>');
-                    for (var i = 0; i < option.limits.length; i++) {
-                        $select.append('<option value="' + option.limits[i] + '" ' + (option.limit == option.limits[i] ? 'selected' : '') + '>' + option.limits[i] + '条每页</option>');
-                    }
-                    $pager.append($select);
-                }
-                if (option.layout[layout_i] == 'jump') {
-                    $pager.append('\
-                    <div class="song-pager-jump">\
-                        <span>到</span>\
-                        <input type="text" class="song-pager-input">\
-                        <span>页</span>\
-                        <button class="song-pager-confirm">确定</button>\
-                    </div>');
-                    $pager.find('.song-pager-input').val(option.nowPage);
-                }
+                option.$count.html('共' + option.count + '条');
+            } else if (option.$count) {
+                option.$count.remove();
+                option.$count = undefined;
             }
+            // 是否显示上一页
+            if (option.layout.indexOf('prev') > -1) {
+                if (!option.$prev) {
+                    option.$prev = $('<a href="javascript:;" class="' + [pagerPrev, (option.nowPage == 1 ? pagerPrevDisabled : '')].join(' ') + '"></a>');
+                    $pager.append(option.$prev);
+                }
+                option.$prev.html(option.prev);
+            } else if (option.$prev) {
+                option.$prev.remove();
+                option.$prev = undefined;
+            }
+            // 是否显示页码
+            if (option.layout.indexOf('page') > -1) {
+                if (!option.$page) {
+                    option.$page = $('<div class="' + pagerNums + '"></div>');
+                    $pager.append(option.$page);
+                }
+            } else if (option.$page) {
+                option.$page.remove();
+                option.$page = undefined;
+            }
+            // 是否显示下一页
+            if (option.layout.indexOf('next') > -1) {
+                if (!option.$next) {
+                    option.$next = $('<a href="javascript:;" class="' + [pagerNext, (option.nowPage == 1 ? pagerNextDisabled : '')].join(' ') + '"></a>');
+                    $pager.append(option.$next);
+                }
+                option.$next.html(option.next);
+            } else if (option.$next) {
+                option.$next.remove();
+                option.$next = undefined;
+            }
+            // 是否显示每页数量
+            if (option.layout.indexOf('limit') > -1) {
+                if (!option.$select) {
+                    option.$select = $('<select class="' + pagerLimit + '" song-ignore></select>');
+                    $pager.append(option.$select);
+                }
+                option.$select.empty();
+                for (var i = 0; i < option.limits.length; i++) {
+                    option.$select.append('<option value="' + option.limits[i] + '" ' + (option.limit == option.limits[i] ? 'selected' : '') + '>' + option.limits[i] + '条每页</option>');
+                }
+            } else if (option.$select) {
+                option.$select.remove();
+                option.$select = undefined;
+            }
+            // 是否显示跳转
+            if (option.layout.indexOf('jump') > -1) {
+                if (!option.$jump) {
+                    option.$jump = $('\
+                    <div class="' + pagerJump + '">\
+                        <span>到</span>\
+                        <input type="text" class="' + pagerInput + '">\
+                        <span>页</span>\
+                        <button class="' + pagerConfirm + '">确定</button>\
+                    </div>');
+                    $pager.append(option.$jump);
+                }
+                option.$jump.find('input.' + pagerInput).val(option.nowPage);
+            } else if (option.$jump) {
+                option.$jump.remove();
+                option.$jump = undefined;
+            }
+            option.$page && renderNowPage(option);
             bindEvent(option);
             // 重置
             option.reload = function (_option) {
@@ -93,52 +150,52 @@
 
         // 渲染当前页码
         function renderNowPage(option) {
-            var $pager = option.$pager;
-            if (option.layout.indexOf('page') > -1) {
-                var $nums = $pager.find('.song-pager-nums');
-                $nums.html('');
-                if (option.pages > option.groups) { // 有省略号
-                    var half = Math.floor((option.groups) / 2);
-                    var start = option.nowPage - half;
-                    start = start + option.groups - 1 >= option.pages ? option.pages - option.groups + 1 : start;
-                    start = start > 0 ? start : 1;
-                    if (option.first !== false) {
-                        if (start > 1) {
-                            $nums.append('<a href="javascript:;" class="song-pager-num" data-page="1">' + (option.first || 1) + '</a>');
-                        }
-                        if (start > 2) { // 左侧省略号
-                            $nums.append('<span class="song-pager-dot">...</span>');
-                        }
+            if (!option.$page) {
+                return;
+            }
+            option.$page.html('');
+            if (option.pages > option.groups) { // 有省略号
+                var half = Math.floor((option.groups) / 2);
+                var start = option.nowPage - half;
+                start = start + option.groups - 1 >= option.pages - 1 ? option.pages - option.groups + 1 : start;
+                start = start == 2 ? 1 : start;
+                start = start > 0 ? start : 1;
+                if (option.first !== false) {
+                    if (start > 1) {
+                        option.$page.append('<a href="javascript:;" class="' + pagerNum + '" data-page="1">' + (option.first || 1) + '</a>');
                     }
-                    for (j = 0; j < option.groups && start <= option.pages; start++, j++) {
-                        $nums.append('<a href="javascript:;" class="song-pager-num" data-page="' + start + '">' + start + '</a>');
-                    }
-                    if (option.last !== false) {
-                        if (start <= option.pages) {
-                            if (start <= option.pages - 1) { //右侧省略号
-                                $nums.append('<span class="song-pager-dot">...</span>');
-                            }
-                            $nums.append('<a href="javascript:;" class="song-pager-num" data-page="' + option.pages + '">' + (option.last || option.pages) + '</a>');
-                        }
-                    }
-                } else {
-                    for (var i = 0; i < option.pages; i++) {
-                        $nums.append('<a href="javascript:;" class="song-pager-num" data-page="' + (i + 1) + '">' + (i + 1) + '</a>');
+                    if (start > 2) { // 左侧省略号
+                        option.$page.append('<span class="' + pagerDot + '">...</span>');
                     }
                 }
-                $pager.find('.song-pager-now').removeClass('song-pager-now');
-                $pager.find('.song-pager-num[data-page="' + option.nowPage + '"]').addClass('song-pager-now');
-            }
-            $pager.find('.song-pager-input').val(option.nowPage);
-            if (option.nowPage == 1) {
-                $pager.find('.song-pager-prev').addClass('song-pager-prev-disabled');
+                for (j = 0; j < option.groups && start <= option.pages; start++, j++) {
+                    option.$page.append('<a href="javascript:;" class="' + pagerNum + '" data-page="' + start + '">' + start + '</a>');
+                }
+                if (option.last !== false) {
+                    if (start <= option.pages) {
+                        if (start <= option.pages - 1) { //右侧省略号
+                            option.$page.append('<span class="' + pagerDot + '">...</span>');
+                        }
+                        option.$page.append('<a href="javascript:;" class="' + pagerNum + '" data-page="' + option.pages + '">' + (option.last || option.pages) + '</a>');
+                    }
+                }
             } else {
-                $pager.find('.song-pager-prev').removeClass('song-pager-prev-disabled');
+                for (var i = 0; i < option.pages; i++) {
+                    option.$page.append('<a href="javascript:;" class="' + pagerNum + '" data-page="' + (i + 1) + '">' + (i + 1) + '</a>');
+                }
+            }
+            option.$page.find('a.' + pagerNow).removeClass(pagerNow);
+            option.$page.find('a.' + pagerNum + '[data-page="' + option.nowPage + '"]').addClass(pagerNow);
+            option.$jump && option.$jump.find('input.' + pagerInput).val(option.nowPage);
+            if (option.nowPage == 1) {
+                option.$prev.addClass(pagerPrevDisabled);
+            } else {
+                option.$prev.removeClass(pagerPrevDisabled);
             }
             if (option.nowPage == option.pages) {
-                $pager.find('.song-pager-next').addClass('song-pager-next-disabled');
+                option.$next.addClass(pagerNextDisabled);
             } else {
-                $pager.find('.song-pager-next').removeClass('song-pager-next-disabled');
+                option.$next.removeClass(pagerNextDisabled);
             }
             // 首次渲染时不需要触发
             if (!option.firstRender) {
@@ -155,41 +212,41 @@
             }
             $pager[0].binded = true;
             // 上一页
-            $pager.delegate('a.song-pager-prev', 'click', function () {
+            $pager.delegate('a.' + pagerPrev, 'click', function () {
                 if (option.nowPage > 1) {
                     option.nowPage--;
                     renderNowPage(option);
                 }
             });
             // 下一页
-            $pager.delegate('a.song-pager-next', 'click', function () {
+            $pager.delegate('a.' + pagerNext, 'click', function () {
                 if (option.nowPage < option.pages) {
                     option.nowPage++;
                     renderNowPage(option);
                 }
             });
             // 点击页码
-            $pager.delegate('a.song-pager-num', 'click', function () {
+            $pager.delegate('a.' + pagerNum, 'click', function () {
                 var $this = $(this);
                 var page = $this.attr('data-page');
                 option.nowPage = page;
                 renderNowPage(option);
             });
             // 跳转
-            $pager.delegate('button.song-pager-confirm', 'click', function () {
-                var page = $pager.find('.song-pager-input').val();
+            $pager.delegate('button.' + pagerConfirm, 'click', function () {
+                var page = option.$jump.find('input.' + pagerInput).val();
                 if (page >= 1 && page <= option.pages) {
                     option.nowPage = page;
                     renderNowPage(option);
                 }
             });
-            $pager.delegate('input.song-pager-input', 'keydown', function (e) {
+            $pager.delegate('input.' + pagerInput, 'keydown', function (e) {
                 if (e.keyCode == 13) {
-                    $pager.find('button.song-pager-confirm').trigger('click');
+                    option.$jump.find('button.' + pagerConfirm).trigger('click');
                 }
             });
             // 每页数量变化
-            $pager.delegate('select.song-pager-limits', 'change', function () {
+            $pager.delegate('select.' + pagerLimit, 'change', function () {
                 var limit = Number($(this).val()) || 0;
                 Pager.trigger('limit(' + option.filter + ')', limit);
                 Pager.trigger('limit', limit);
