d46eee7f75e6d07d64172480c1a289c207b2d2a9
DESKTOP-SI1F6AQ\40905 <409051987@qq.com>
2021-08-05 21:55:00 +0800

    [master]

--- modules/transfer.js
+++ modules/transfer.js
@@ -37,16 +37,19 @@
         }
         var transferClass = {
             transfer: 'song-transfer',
+            left: 'song-transfer-left',
+            right: 'song-transfer-right',
             leftBody: 'song-transfer-left-body',
             rightBody: 'song-transfer-right-body',
+            item: 'song-transfer-item',
             title: 'song-transfer-title',
+            search: 'song-transfer-search',
             btns: 'song-transfer-btns',
             btn: 'song-transfer-btn',
             activeBtn: 'song-transfer-btn-active',
             checkbox: 'song-transfer-checkbox',
             checked: 'song-transfer-checked',
             disabled: 'song-transfer-disabled',
-            item: 'song-transfer-item',
             empty: 'song-transfer-empty'
         }
 
@@ -83,10 +86,16 @@
         }
 
         Class.prototype.render = function () {
+            // 自定义配置-begin
+            this.stretch = this.option.stretch || false;
+            this.search = this.option.search || false;
+            this.width = this.option.width || 200;
+            this.height = this.option.height || 320;
+            // 自定义配置-end
             this.$elem = $(this.option.elem);
             this.$elem = this.$elem.length ? this.$elem : $docBody;
-            this.originData = Common.deepAssign([], this.option.data);
-            this.value = Common.deepAssign([], this.option.value);
+            this.leftData = Common.deepAssign([], this.option.data);
+            this.rightData = Common.deepAssign([], this.option.value);
             this.$transfer = $(tpl.transfer);
             this.$left = $(Common.htmlTemplate(tpl.left, {
                 title: this.option.leftTitle || '列表1'
@@ -98,6 +107,8 @@
             this.$rightBody = this.$right.children('.' + transferClass.rightBody);
             this.$leftTitle = this.$left.children('.' + transferClass.title);
             this.$rightTitle = this.$right.children('.' + transferClass.title);
+            this.$leftSearch = this.$left.children('.' + transferClass.search);
+            this.$rightSearch = this.$right.children('.' + transferClass.search);
             this.$leftBtn = $(tpl.leftBtn);
             this.$rightBtn = $(tpl.rightBtn);
             this.$center = $(tpl.center);
@@ -105,18 +116,19 @@
             this.dataMap = {};
             this.key = 1;
             this.showList = [];
-            this.stretch = this.option.stretch || false;
-            this.width = this.option.width || 200;
-            this.height = this.option.height || 320;
+            this.appendItem(this.leftData, this.$leftBody);
+            this.appendItem(this.rightData, this.$rightBody);
+            this.setAllChecked();
+            this.setBtnActive();
+            this.setEmpty();
             if (this.stretch) {
                 this.width = this.$elem[0].clientWidth || this.$elem[0].offsetWidth;
                 this.width = (this.width - 84) / 2;
             }
-            this.appendItem(this.originData, this.$leftBody);
-            this.appendItem(this.value, this.$rightBody);
-            this.setBtnActive();
-            this.setAllChecked();
-            this.setEmpty();
+            if (this.search) {
+                this.$leftSearch.show();
+                this.$rightSearch.show();
+            }
             this.$left.css({
                 'width': this.width,
                 'height': this.height
@@ -157,6 +169,7 @@
                 }
                 that.dataMap[item._song_key] = item;
                 $parent.append($item);
+                item._song_hidden = false;
                 item._song_checked = item._song_checked || item.checked && !item.disabled;
                 item._song_disabled = item._song_disabled || item.disabled;
                 if (item._song_checked) {
@@ -195,11 +208,11 @@
             this.$transfer.delegate('.' + transferClass.title, 'click', function () {
                 var $this = $(this);
                 var checked = false;
-                var $tansferBody = $this.next('.' + transferClass.leftBody);
-                var dataList = that.getEnable(that.originData);
+                var $tansferBody = $this.parent('.' + transferClass.left).children('.' + transferClass.leftBody);
+                var dataList = that.getEnable(that.leftData);
                 if (!$tansferBody.length) {
                     $tansferBody = that.$rightBody;
-                    dataList = that.getEnable(that.value);
+                    dataList = that.getEnable(that.rightData);
                 }
                 checked = that.getChecked(dataList).length != dataList.length;
                 checked ? $this.addClass(transferClass.checked) : $this.removeClass(transferClass.checked);
@@ -211,7 +224,7 @@
                 $tansferBody.children('.' + transferClass.item).each(function (i, item) {
                     var $item = $(item);
                     var _data = that.dataMap[$item.attr('data-key')];
-                    if (!_data._song_disabled) {
+                    if (!_data._song_disabled && !_data._song_hidden) {
                         checked ? $item.addClass(transferClass.checked) : $item.removeClass(transferClass.checked);
                     }
                 });
@@ -219,18 +232,27 @@
             });
             // 往左移动事件
             this.$leftBtn.on('click', function () {
-                var checkedData = that.getChecked(that.value);
+                var checkedData = that.getChecked(that.rightData);
                 if (checkedData.length) {
-                    that.originData = that.originData.concat(checkedData);
-                    that.value = that.value.filter(function (item) {
+                    that.leftData = that.leftData.concat(checkedData);
+                    that.rightData = that.rightData.filter(function (item) {
                         return !item._song_checked
                     });
+                    // 左侧搜索框有内容，需要过滤
                     checkedData.map(function (item) {
                         item._song_checked = false;
+                        if (that.leftSearchText && item.title.indexOf(that.leftSearchText) == -1) {
+                            item._song_hidden = true;
+                        } else {
+                            item._song_hidden = false;
+                        }
                     });
                     that.$rightBody.children('.' + transferClass.checked).each(function (i, item) {
-                        $(item).removeClass(transferClass.checked);
-                        that.$leftBody.append(item);
+                        var $item = $(item);
+                        var data = that.dataMap[$item.attr('data-key')];
+                        $item.removeClass(transferClass.checked);
+                        that.$leftBody.append($item);
+                        data._song_hidden && $item.hide();
                     });
                     $(this).removeClass(transferClass.activeBtn);
                     that.$leftTitle.removeClass(transferClass.checked);
@@ -238,7 +260,7 @@
                     that.setEmpty();
                     // 触发change事件
                     that.trigger('change', {
-                        data: that.value.map(function (item) {
+                        data: that.rightData.map(function (item) {
                             return Common.delInnerProperty(item);
                         }),
                         dom: this
@@ -247,18 +269,27 @@
             });
             // 往右移动事件
             this.$rightBtn.on('click', function () {
-                var checkedData = that.getChecked(that.originData);
+                var checkedData = that.getChecked(that.leftData);
                 if (checkedData.length) {
-                    that.value = that.value.concat(checkedData);
-                    that.originData = that.originData.filter(function (item) {
+                    that.rightData = that.rightData.concat(checkedData);
+                    that.leftData = that.leftData.filter(function (item) {
                         return !item._song_checked
                     });
                     checkedData.map(function (item) {
                         item._song_checked = false;
+                        // 右侧搜索框有内容，需要过滤
+                        if (that.rightSearchText && item.title.indexOf(that.rightSearchText) == -1) {
+                            item._song_hidden = true;
+                        } else {
+                            item._song_hidden = false;
+                        }
                     });
                     that.$leftBody.children('.' + transferClass.checked).each(function (i, item) {
-                        $(item).removeClass(transferClass.checked);
-                        that.$rightBody.append(item);
+                        var $item = $(item);
+                        var data = that.dataMap[$item.attr('data-key')];
+                        $item.removeClass(transferClass.checked);
+                        that.$rightBody.append($item);
+                        data._song_hidden && $item.hide();
                     });
                     $(this).removeClass(transferClass.activeBtn);
                     that.$leftTitle.removeClass(transferClass.checked);
@@ -266,23 +297,65 @@
                     that.setEmpty();
                     // 触发change事件
                     that.trigger('change', {
-                        data: that.value.map(function (item) {
+                        data: that.rightData.map(function (item) {
                             return Common.delInnerProperty(item);
                         }),
                         dom: this
                     });
                 }
             });
+            // 左边搜索框搜索事件
+            this.$leftSearch.find('input').on('input propertychange', function () {
+                that.leftSearchText = this.value;
+                clearTimeout(that.bindEvent.inputTimer);
+                that.bindEvent.inputTimer = setTimeout(function () {
+                    that.leftData.map(function (item) {
+                        if (that.leftSearchText && item.title.indexOf(that.leftSearchText) == -1) {
+                            item._song_hidden = true;
+                        } else {
+                            item._song_hidden = false;
+                        }
+                    });
+                    that.$leftBody.children('.' + transferClass.item).each(function (i, item) {
+                        var $item = $(item);
+                        var data = that.dataMap[$item.attr('data-key')];
+                        data._song_hidden ? $item.hide() : $item.show();
+                    });
+                    that.setAllChecked();
+                    that.setBtnActive();
+                }, 50);
+            });
+            // 右边搜索框搜索事件
+            this.$rightSearch.find('input').on('input propertychange', function () {
+                that.rightSearchText = this.value;
+                clearTimeout(that.bindEvent.inputTimer);
+                that.bindEvent.inputTimer = setTimeout(function () {
+                    that.leftData.map(function (item) {
+                        if (that.rightSearchText && item.title.indexOf(that.rightSearchText) == -1) {
+                            item._song_hidden = true;
+                        } else {
+                            item._song_hidden = false;
+                        }
+                    });
+                    that.$rightBody.children('.' + transferClass.item).each(function (i, item) {
+                        var $item = $(item);
+                        var data = that.dataMap[$item.attr('data-key')];
+                        data._song_hidden ? $item.hide() : $item.show();
+                    });
+                    that.setAllChecked();
+                    that.setBtnActive();
+                });
+            });
         }
 
         // 按钮是否可用
         Class.prototype.setBtnActive = function () {
-            if (this.getChecked(this.originData).length > 0) {
+            if (this.getChecked(this.leftData).length > 0) {
                 this.$rightBtn.addClass(transferClass.activeBtn);
             } else {
                 this.$rightBtn.removeClass(transferClass.activeBtn);
             }
-            if (this.getChecked(this.value).length > 0) {
+            if (this.getChecked(this.rightData).length > 0) {
                 this.$leftBtn.addClass(transferClass.activeBtn);
             } else {
                 this.$leftBtn.removeClass(transferClass.activeBtn);
@@ -293,15 +366,15 @@
         Class.prototype.setAllChecked = function () {
             var checkedData = null;
             var enableData = null;
-            checkedData = this.getChecked(this.originData);
-            enableData = this.getEnable(this.originData);
+            checkedData = this.getChecked(this.leftData);
+            enableData = this.getEnable(this.leftData);
             if (checkedData.length == enableData.length && enableData.length) {
                 this.$leftTitle.addClass(transferClass.checked);
             } else {
                 this.$leftTitle.removeClass(transferClass.checked);
             }
-            checkedData = this.getChecked(this.value);
-            enableData = this.getEnable(this.value);
+            checkedData = this.getChecked(this.rightData);
+            enableData = this.getEnable(this.rightData);
             if (checkedData.length == enableData.length && enableData.length) {
                 this.$rightTitle.addClass(transferClass.checked);
             } else {
@@ -312,13 +385,13 @@
         // 列表是否为空
         Class.prototype.setEmpty = function () {
             var $empty = this.$leftBody.children('.' + transferClass.empty);
-            if (this.originData.length) {
+            if (this.leftData.length) {
                 $empty.hide();
             } else {
                 $empty.show();
             }
             $empty = this.$rightBody.children('.' + transferClass.empty);
-            if (this.value.length) {
+            if (this.rightData.length) {
                 $empty.hide();
             } else {
                 $empty.show();
@@ -333,7 +406,7 @@
         // 获取选中的数据
         Class.prototype.getChecked = function (data) {
             var result = data.filter(function (item) {
-                return item._song_checked;
+                return item._song_checked && !item._song_hidden;
             });
             return result;
         }
@@ -341,7 +414,7 @@
         // 获取可用的数据
         Class.prototype.getEnable = function (data) {
             var result = data.filter(function (item) {
-                return !item._song_disabled;
+                return !item._song_disabled && !item._song_hidden;
             });
             return result;
         }
@@ -349,7 +422,7 @@
         // 获取移动的数据
         Class.prototype.getTransfered = function (data) {
             var result = [];
-            this.value.map(function (item) {
+            this.rightData.map(function (item) {
                 result.push(Common.deepAssign({}, item));
             });
             return result;
