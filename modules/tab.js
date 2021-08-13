6e6b9e7d9440282031a6f0c06ea0e74ebdaf2205
DESKTOP-SI1F6AQ\40905 <409051987@qq.com>
2021-08-13 15:19:55 +0800

    [master]

--- modules/tab.js
+++ modules/tab.js
@@ -3,15 +3,29 @@
         var leftIcon = '&#xe733;';
         var rightIcon = '&#xe734;';
         var tpl = {
-            title: '<div class="song-tab-title"></div>',
+            tab: '<div class="song-tab song-tab-structure">\
+                <div class="song-tab-header">\
+                    <div class="song-tab-header-scroll">\
+                        <div class="song-tab-titles">\
+                        </div>\
+                    </div>\
+                </div>\
+                <div class="song-tab-content">\
+                </div>\
+            </div>',
+            item: '<div class="song-tab-item" name="<%-name%>"><%-content%></div>',
+            title: '<div class="song-tab-title" name="<%-name%>"><%-title%></div>',
             prev: '<div class="song-tab-prev">' + leftIcon + '</div>',
             next: '<div class="song-tab-next">' + rightIcon + '</div>'
         }
         var tabClass = {
             tab: 'song-tab',
+            structure: 'song-tab-structure',
+            scroll: 'song-tab-scroll',
             header: 'song-tab-header',
             headerScroll: 'song-tab-header-scroll',
             content: 'song-tab-content',
+            titles: 'song-tab-titles',
             title: 'song-tab-title',
             item: 'song-tab-item',
             titleActive: 'song-tab-title-active',
@@ -24,32 +38,64 @@
         // 页码类
         function Class(option) {
             this.option = Object.assign({}, option);
-            this.$tab = this.option.$tab;
             this.render();
         }
 
         Class.prototype.render = function () {
-            var $header = this.$tab.children('.' + tabClass.header);
-            var $headerScroll = $header.children('.' + tabClass.headerScroll);
-            if ($header[0].scrollWidth > $header[0].clientWidth) {
-                var $prev = $header.children('.' + tabClass.prev);
-                var $next = $header.children('.' + tabClass.next);
+            this.$tab = this.option.$tab || $(tpl.tab);
+            this.data = this.option.data || [];
+            this.style = this.option.style || 'card';
+            this.$header = this.$tab.children('.' + tabClass.header);
+            this.$headerScroll = this.$header.children('.' + tabClass.headerScroll);
+            this.$titles = this.$headerScroll.children('.' + tabClass.titles);
+            this.$content = this.$tab.children('.' + tabClass.content);
+            if (!this.option.$tab) {
+                this.appendItem();
+            }
+            this.checkScroll();
+            this.bindEvent();
+        }
+
+        Class.prototype.appendItem = function () {
+            var that = this;
+            this.data.map(function (tab) {
+                that.$titles.append(Common.htmlTemplate(tpl.title, {
+                    title: tab.title,
+                    name: tab.name
+                }));
+                that.$content.append(Common.htmlTemplate(tpl.item, {
+                    content: tab.content,
+                    name: tab.name
+                }));
+            });
+        }
+
+        Class.prototype.addTab = function (tab) {
+            this.$titles.append(Common.htmlTemplate(tpl.title, {
+                title: tab.title,
+                name: tab.name
+            }));
+            this.checkScroll();
+        }
+
+        Class.prototype.removeTab = function (name) {
+            this.$titles.children('[name="' + name + '"]').remove();
+            this.checkScroll();
+        }
+
+        // 检查是否出现滚动条
+        Class.prototype.checkScroll = function () {
+            if (this.$headerScroll[0].scrollWidth > this.$headerScroll[0].clientWidth) {
+                var $prev = this.$header.children('.' + tabClass.prev);
+                var $next = this.$header.children('.' + tabClass.next);
                 if (!$prev.length) {
-                    $header.append(tpl.prev);
+                    this.$header.append(tpl.prev);
                 }
                 if (!$next.length) {
-                    $header.append(tpl.next);
+                    this.$header.append(tpl.next);
                 }
-                $header.css({
-                    'padding-left': '40px',
-                    'padding-right': '40px'
-                });
-                $headerScroll.css({
-                    'position': 'absolute',
-                    'left': 40
-                })
+                this.$tab.addClass(tabClass.scroll);
             }
-            this.bindEvent();
         }
 
         Class.prototype.bindEvent = function () {
@@ -57,12 +103,10 @@
             this.$tab.delegate('.' + tabClass.title, 'click', function () {
                 var $this = $(this);
                 var tabName = $this.attr('name');
-                var $headerScroll = that.$tab.children('.' + tabClass.header).children('.' + tabClass.headerScroll);
-                var $content = that.$tab.children('.' + tabClass.content);
-                $headerScroll.children('.' + tabClass.titleActive).removeClass(tabClass.titleActive);
-                $content.children('.' + tabClass.itemActive).removeClass(tabClass.itemActive);
+                that.$titles.children('.' + tabClass.titleActive).removeClass(tabClass.titleActive);
+                that.$content.children('.' + tabClass.itemActive).removeClass(tabClass.itemActive);
                 $this.addClass(tabClass.titleActive);
-                $content.children('[target="' + tabName + '"]').addClass(tabClass.itemActive);
+                that.$content.children('[target="' + tabName + '"]').addClass(tabClass.itemActive);
             });
             this.$tab.delegate('.' + tabClass.prev, 'click', function () {
                 that.prev();
@@ -74,17 +118,16 @@
 
         // 前一页
         Class.prototype.prev = function () {
-            var $header = this.$tab.children('.' + tabClass.header);
-            var $headerScroll = $header.children('.' + tabClass.headerScroll);
-            var left = $headerScroll[0].offsetLeft;
-            var headerWidth = $header[0].clientWidth;
-            $headerScroll.children('.' + tabClass.title).each(function (i, title) {
-                if (title.offsetLeft + left >= 40) {
-                    left = title.offsetLeft - (headerWidth - 40);
-                    if (left < -40) {
-                        left = -40;
+            var that = this;
+            var left = this.$titles[0].offsetLeft;
+            var headerWidth = this.$headerScroll[0].clientWidth;
+            this.$titles.children('.' + tabClass.title).each(function (i, title) {
+                if (title.offsetLeft + left >= 0) {
+                    left = title.offsetLeft - headerWidth;
+                    if (left < 0) {
+                        left = 0;
                     }
-                    $headerScroll.css('left', -left);
+                    that.$titles.css('left', -left);
                     return false;
                 }
             });
@@ -92,24 +135,23 @@
 
         // 后一页
         Class.prototype.next = function () {
-            var $header = this.$tab.children('.' + tabClass.header);
-            var $headerScroll = $header.children('.' + tabClass.headerScroll);
-            var left = $headerScroll[0].offsetLeft;
-            var headerWidth = $header[0].clientWidth;
-            var scrollWidth = $headerScroll[0].clientWidth;
-            $headerScroll.children('.' + tabClass.title).each(function (i, title) {
-                if (title.offsetLeft + title.offsetWidth + left >= headerWidth - 40) {
-                    left = title.offsetLeft - 40;
-                    if (left > scrollWidth - (headerWidth - 40)) {
-                        left = scrollWidth - (headerWidth - 40);
+            var that = this;
+            var left = this.$titles[0].offsetLeft;
+            var headerWidth = this.$headerScroll[0].clientWidth;
+            var scrollWidth = this.$titles[0].clientWidth;
+            this.$titles.children('.' + tabClass.title).each(function (i, title) {
+                if (title.offsetLeft + title.offsetWidth + left >= headerWidth) {
+                    left = title.offsetLeft;
+                    if (left > scrollWidth - headerWidth) {
+                        left = scrollWidth - headerWidth;
                     }
-                    $headerScroll.css('left', -left);
+                    that.$titles.css('left', -left);
                     return false;
                 }
             });
         }
 
-        function init() {
+        function check() {
             $('div.' + tabClass.tab).each(function (i, tab) {
                 var $tab = $(tab);
                 if (!$tab.hasClass(tabClass.structure)) {
@@ -120,19 +162,21 @@
             });
         }
 
-        init();
+        check();
 
         var SongTab = {
             on: event.on,
             once: event.once,
             trigger: event.trigger,
+            check: check,
             render: function (option) {
                 var tab = new Class(option);
                 return {
                     on: tab.on,
                     once: tab.once,
                     trigger: tab.trigger,
-                    init: init
+                    addTab: tab.addTab.bind(tab),
+                    removeTab: tab.removeTab.bind(tab)
                 }
             }
         }
