2ac8d75f455892780587a3d74abf803c4c5bbbd0
DESKTOP-SI1F6AQ\40905 <409051987@qq.com>
2021-09-23 14:11:38 +0800

    [master]

---
+++ build/app.build.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: lisong
+ * @Date: 2021-03-16 16:09:29
+ * @Description: 
+ */
+({
+    baseUrl: '../',
+    name: 'index',
+    out: '../jy-ui.all.js',
+    map: {
+        '*': {
+            'css': 'lib/require-css/css.min'
+        }
+    }
+})
---
+++ index.js
@@ -0,0 +1,36 @@
+/*
+ * @Author: lisong
+ * @Date: 2021-08-13 15:51:50
+ * @Description: 
+ */
+if ("function" == typeof define && define.amd) {
+    define([
+        'modules/common',
+        'modules/date',
+        'modules/dialog',
+        'modules/form',
+        'modules/Select',
+        'modules/menu-nav',
+        'modules/pager',
+        'modules/tab',
+        'modules/table',
+        'modules/transfer',
+        'modules/tree',
+        'modules/upload'
+    ], function (Common, _Date, Dialog, Form, Select, MenuNav, Pager, Tab, Table, Transfer, Tree, Upload) {
+        return {
+            Common: Common,
+            Date: _Date,
+            Dialog: Dialog,
+            Form: Form,
+            Select: Select,
+            MenuNav: MenuNav,
+            Pager: Pager,
+            Tab: Tab,
+            Table: Table,
+            Transfer: Transfer,
+            Tree: Tree,
+            Upload: Upload
+        }
+    });
+}
